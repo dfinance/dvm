576de522d972a731ce389ed29a7286f135f7ec44
Maksim Kurnikov <maxim.kurnikov@gmail.com>
2020-02-10 16:46:18 +0300

    Return error codes (#31)
    * enable logging for vm
    
    * add error codes
    
    * upd sub-module
    
    Co-authored-by: Alexander Koz. <a@koz.email>

--- src/bin/ds-server.rs
+++ src/bin/ds-server.rs
@@ -29,6 +29,19 @@ pub struct DataSourceService {
 fn new_response(blob: &[u8]) -> Response<DsRawResponse> {
     Response::new(DsRawResponse {
         blob: blob.to_vec(),
+        error_code: ds_raw_response::ErrorCode::None as i32,
+        error_message: vec![],
+    })
+}
+
+fn new_error_response(
+    error_code: ds_raw_response::ErrorCode,
+    error_message: String,
+) -> Response<DsRawResponse> {
+    Response::new(DsRawResponse {
+        blob: vec![],
+        error_code: error_code as i32,
+        error_message: error_message.into_bytes(),
     })
 }
 
