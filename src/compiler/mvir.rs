0fa33d692f2c871ad6bc6534bacf97c3af09ab47
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2020-02-18 23:04:40 +0300

    address review comments

--- src/compiler/mvir.rs
+++ src/compiler/mvir.rs
@@ -9,8 +9,9 @@ use tonic::transport::Channel;
 use vm::CompiledModule;
 
 use crate::compiled_protos::ds_grpc::{DsAccessPath, DsRawResponse};
+use crate::compiled_protos::ds_grpc::ds_raw_response::ErrorCode;
 use crate::compiled_protos::ds_grpc::ds_service_client::DsServiceClient;
-use crate::compiled_protos::vm_grpc::{CompilationResult, MvIrSourceFile};
+use crate::compiled_protos::vm_grpc::{CompilationResult, ContractType, MvIrSourceFile};
 use crate::compiled_protos::vm_grpc::vm_compiler_server::VmCompiler;
 
 pub fn extract_imports(source_text: &str, is_module: bool) -> Result<Vec<AccessPath>> {
@@ -87,7 +88,9 @@ impl CompilerService {
         let source_file_data = request.into_inner();
 
         let source_text = source_file_data.text;
-        let is_module = source_file_data.is_module;
+        let is_module = ContractType::from_i32(source_file_data.r#type)
+            .expect("Invalid ContractType")
+            == ContractType::Module;
 
         let imports = match extract_imports(&source_text, is_module) {
             Ok(imports) => imports,
@@ -111,30 +114,31 @@ impl CompilerService {
                 )));
             }
             let ds_response = response.unwrap().into_inner();
-            match ds_response.error_code {
-                0 => {
+            let error_code =
+                ErrorCode::from_i32(ds_response.error_code).expect("DS returned invalid ErrorCode");
+            match error_code {
+                ErrorCode::None => {
                     let resolved_dep_module = CompiledModule::deserialize(&ds_response.blob)
                         .expect("Module deserialization failed");
                     deps.push(VerifiedModule::new(resolved_dep_module).unwrap());
                 }
-                // BadRequest, should not happen
-                1 => panic!("DS returned BAD_REQUEST"),
+                // should not happen
+                ErrorCode::BadRequest => panic!("DS returned BAD_REQUEST"),
                 // NoData, compiler error: cannot resolve a dependency
-                2 => {
+                ErrorCode::NoData => {
                     dependency_errors.push(String::from_utf8(ds_response.error_message).unwrap());
                 }
-                _ => panic!("DS returned invalid ErrorCode"),
             };
         }
         if !dependency_errors.is_empty() {
             return Ok(Err(dependency_errors));
         }
 
-        let address_lit = match String::from_utf8(source_file_data.address.to_vec()) {
+        let address_lit = match std::str::from_utf8(&source_file_data.address) {
             Ok(address) => address,
             Err(_) => return Err(Status::invalid_argument("Address is not a valid utf8")),
         };
-        let account_address = AccountAddress::from_hex_literal(&address_lit).unwrap();
+        let account_address = AccountAddress::from_hex_literal(address_lit).unwrap();
 
         let mut compiler = compiler::Compiler::default();
         compiler.skip_stdlib_deps = true;
