d671ef7f1b9844456dfe6aa43e3289142a50b4c9
Dmitry Yakushev <rig410@yandex.ru>
2020-03-26 20:16:17 +0300

    Changed oracles. Added x"" literal pattern. (#69)
    * Changed oracles. Added x"" literal pattern.
    * changed oracle ticker prefix.

--- lang/src/lib.rs
+++ lang/src/lib.rs
@@ -6,3 +6,11 @@ pub mod banch32;
 pub mod bytecode;
 pub mod compiler;
 pub mod stdlib;
+
+#[macro_export]
+macro_rules! pattern {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
