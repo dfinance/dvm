372047dd3af95d1fc7fbf2bcc2ed7bbde959ac6d
Dm. Yakushev <rig410@yandex.ru>
2020-05-15 12:50:27 +0300

    Added status table util.

---
+++ cli/src/bin/status-table.rs
@@ -0,0 +1,40 @@
+use std::path::PathBuf;
+use libra::libra_types::vm_error::StatusCode;
+use structopt::StructOpt;
+use std::collections::HashMap;
+use enum_iterator::IntoEnumIterator;
+use anyhow::Result;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+#[derive(StructOpt)]
+struct Opts {
+    /// Optional path to the output file.
+    /// If not passed, result will be printed to stdout.
+    #[structopt(short, long, parse(from_os_str))]
+    output: Option<PathBuf>,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+    let status_table = status_table_json().unwrap();
+    if let Some(output) = opts.output {
+        let mut f = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(&output)
+            .unwrap();
+        f.set_len(0).unwrap();
+        f.write_all(status_table.as_bytes()).unwrap();
+    } else {
+        println!("{}", status_table);
+    }
+}
+
+fn status_table_json() -> Result<String> {
+    Ok(serde_json::to_string_pretty(
+        &StatusCode::into_enum_iter()
+            .map(|code| (code as u64, format!("{:?}", code)))
+            .collect::<HashMap<_, _>>(),
+    )?)
+}
