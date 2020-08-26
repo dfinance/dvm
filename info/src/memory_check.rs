91507513b6ba3897f3a0b925c07427010308fc14
Dm. Yakushev <rig410@yandex.ru>
2020-08-26 11:20:52 +0300

    added mem checker test.

--- info/src/memory_check.rs
+++ info/src/memory_check.rs
@@ -29,7 +29,7 @@ impl MemoryChecker {
         let count = self.counter.fetch_add(1, Ordering::SeqCst);
 
         if count == 0 {
-            let idle_memory = get_system_metrics().memory as usize;
+            let idle_memory = current_memory();
             *self
                 .idle_memory
                 .write()
@@ -45,16 +45,16 @@ impl MemoryChecker {
                 warn!("Unexpected cache size.{:?}. Expected that the cache size is less than the process memory usages.", external_cache_size);
                 false
             } else {
-                let available_memory = mem - external_cache_size;
+                let used_memory = mem - external_cache_size;
                 let idle_memory = self
                     .idle_memory
                     .read()
                     .unwrap_or_else(|err| err.into_inner())
                     .unwrap_or(0);
-                if idle_memory > available_memory {
+                if idle_memory > used_memory {
                     false
                 } else {
-                    self.max_memory > available_memory - idle_memory
+                    self.max_memory < used_memory - idle_memory
                 }
             }
         } else {
@@ -65,11 +65,67 @@ impl MemoryChecker {
 
 /// Returns current memory usages.
 fn current_memory() -> usize {
-    get_system_metrics().memory as usize
+    (get_system_metrics().memory * 1024) as usize
 }
 
 /// Provide cache size function.
 pub trait CacheSize {
     /// Returns cache size.
     fn size(&self) -> usize;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::memory_check::{MemoryChecker, CacheSize};
+    use crate::config::MemoryOptions;
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[derive(Clone)]
+    struct FakeCache(Rc<RefCell<Vec<u8>>>);
+
+    impl FakeCache {
+        fn new() -> FakeCache {
+            FakeCache {
+                0: Rc::new(RefCell::new(vec![])),
+            }
+        }
+
+        fn fill(&self, bytes_count: usize) {
+            let mut buffer = self.0.borrow_mut();
+            for byte in 0..bytes_count {
+                buffer.push(byte as u8);
+            }
+        }
+    }
+
+    impl CacheSize for FakeCache {
+        fn size(&self) -> usize {
+            self.0.borrow().len()
+        }
+    }
+
+    #[test]
+    fn test_checker() {
+        let cache = FakeCache::new();
+        let cache_2 = FakeCache::new();
+        let checker = MemoryChecker::new(
+            MemoryOptions {
+                module_cache: 10000,
+                memory_check_period: 1000,
+                max_dvm_cache_size: 2000,
+            },
+            vec![Box::new(cache.clone())],
+        );
+
+        loop {
+            if checker.is_limit_exceeded() {
+                break;
+            } else {
+                cache_2.fill(1024);
+                cache.fill(1024);
+            }
+        }
+        assert_eq!(cache_2.size(), 1024 * 1000);
+    }
 }
