e078e9a05cae88ac8025562d3a926df8dd7e14b5
Alexander Koz <a@koz.email>
2020-09-28 12:47:49 +0300

    add output option standalone move disassembler

--- compiler/src/bin/move-disassembler.rs
+++ compiler/src/bin/move-disassembler.rs
@@ -1,6 +1,8 @@
 extern crate clap;
 
 use std::path::PathBuf;
+use std::fs::{File, canonicalize, read};
+use std::io::Write;
 use clap::Clap;
 use anyhow::Error;
 use dvm_compiler::disassembler;
@@ -11,21 +13,35 @@ struct Opt {
     #[clap(about = "Path to input file", long, short)]
     /// Path to compiled Move binary
     input: PathBuf,
+
+    #[clap(about = "Path to output file", long, short)]
+    /// Optional path to output file.
+    /// Prints results to stdout by default.
+    output: Option<PathBuf>,
 }
 
-fn main() -> Result<(), Error> {
-    // let current_dir = env::current_dir().unwrap();
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+    }
+}
+
+fn run() -> Result<(), Error> {
     let opts = Opt::parse();
 
-    let input_path = std::fs::canonicalize(opts.input)?;
-    let input_bytes = std::fs::read(input_path)?;
+    let input_path = canonicalize(opts.input)?;
+    let input_bytes = read(input_path)?;
 
     let cfg = disassembler::Config {
         light_version: false,
     };
     let out = disassembler::disasm_str(&input_bytes, cfg)?;
 
-    println!("{}", out);
+    if let Some(output_path) = opts.output {
+        File::create(output_path)?.write_all(out.as_bytes())?;
+    } else {
+        println!("{}", out);
+    }
 
     Ok(())
 }
